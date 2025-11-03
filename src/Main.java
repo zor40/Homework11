1198f79be5cfc2a33a728ae3790eda49333fd438
zor40 <zor40@mail.ru>
2025-11-04 04:26:44 +0500

    Сделано ДЗ

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,59 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
+import java.time.LocalDate;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        System.out.println("Task 1");
+        leapYear(2024);
+        System.out.println();
+        System.out.println("Task 2");
+        operatingSystem(0, 2014);
+        System.out.println();
+        System.out.println("Task 3");
+        delivery(95, 1);
+        System.out.println();
+    }
 
+    public static void leapYear(int year) {
+        if (year > 1584 && ((year % 400 == 0) || (year % 4 == 0 && year % 100 != 0))) {
+            System.out.println(year + " год является високосным.");
+        } else if (year > 1584) {
+            System.out.println(year + " год не является високосным.");
+        } else {
+            System.out.println("Год должен быть больше, чем 1584.");
+        }
+    }
+
+    public static void operatingSystem(int clientOperatingSystem, int clientDeviceYear) {
+        if (clientOperatingSystem == 0 && clientDeviceYear >= 2015) {
+            System.out.println("Установите версию приложения для iOS по ссылке.");
+        } else if (clientOperatingSystem == 0 && clientDeviceYear < 2015) {
+            System.out.println("Установите облегченную версию приложения для iOS по ссылке.");
+        } else if (clientOperatingSystem == 1 && clientDeviceYear >= 2015) {
+            System.out.println("Установите версию приложения для Android по ссылке.");
+        } else {
+            System.out.println("Установите облегченную версию приложения для Android по ссылке.");
+        }
+    }
+
+    public static void delivery(int deliveryDistance, int deliveryDay) {
+        if (deliveryDistance < 20) {
+            System.out.println("Потребуется " + deliveryDay + " день.");
+        }
+        if (deliveryDistance > 20 && deliveryDistance <= 60) {
+            deliveryDay += 1;
+            System.out.println("Потребуется " + deliveryDay + " дня.");
+        }
+        if (deliveryDistance > 60 && deliveryDistance <= 100) {
+            deliveryDay += 2;
+            System.out.println("Потребуется " + deliveryDay + " дня.");
+        }
+        if (deliveryDistance > 100) {
+            System.out.println("Доставки нет.");
+        }
     }
 }
+
+
